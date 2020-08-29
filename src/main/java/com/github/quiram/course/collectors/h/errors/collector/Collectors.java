8e7e5d7dccf15509a579a9b682d7dfd3bffe895a
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2020-08-29 20:24:45 +0100

    add toListOf(Class<T>) collector

---
+++ src/main/java/com/github/quiram/course/collectors/h/errors/collector/Collectors.java
@@ -0,0 +1,28 @@
+package com.github.quiram.course.collectors.h.errors.collector;
+
+import java.util.List;
+import java.util.stream.Collector;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+public class Collectors {
+    public static <P, T extends P> Collector<P, ?, List<T>> toListOf(Class<T> klass) {
+        return collectingAndThen(
+                toList(),
+                list -> {
+                    final List<P> offendingItems = list.stream()
+                            .filter(x -> !klass.isAssignableFrom(x.getClass()))
+                            .collect(toList());
+
+                    if (offendingItems.isEmpty()) {
+                        return list.stream()
+                                .map(klass::cast)
+                                .collect(toList());
+                    }
+
+                    throw new RuntimeException(format("The following items cannot be cast to %s: %s", klass.getSimpleName(), offendingItems));
+                });
+    }
+}
